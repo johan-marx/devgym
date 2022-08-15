aceaf4f8bdc6fa681ee3586e602bdb7ff13885a5
Johan Marx <johan.marx@stratusolve.com>
2022-08-15 09:11:25 +0200

    Added exercises, updated index

--- generate_index.php
+++ generate_index.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Code Warrior Index</h1>
     <?php
-        $dir = 'Exercises';
+    $dir = 'Exercises';
 
-        $folders = scandir($dir, 1);
-        echo '<ul>';
-        foreach($folders as $folder) {
-            if (!strstr($folder, '.')) {
-                $date = DateTime::createFromFormat('Y_m_d', $folder);
-                echo '<li>'.$date->format('d F Y').'</li>';
-                $files = scandir($dir.'/'.$folder.'/', 1);
-                foreach ($files as $file) {
-                    if (strlen($file) > 2) {
-                        echo '<ul>';
-                            echo '<li><a href="'.$dir.'/'.$folder.'/'.$file.'" target="_blank">'.$file.'</a></li>';
-                        echo '</ul>';
-                    }
-
-                } 
+    $folders = scandir($dir, 1);
+    echo '<ul>';
+    foreach ($folders as $folder) {
+        if (!strstr($folder, '.')) {
+            $date = DateTime::createFromFormat('Y_m_d', $folder);
+            echo '<li>' . $date->format('d F Y') . '</li>';
+            $files = scandir($dir . '/' . $folder . '/', 1);
+            foreach ($files as $file) {
+                if (strlen($file) > 2) {
+                    echo '<ul>';
+                    echo '<li><a href="' . $dir . '/' . $folder . '/' . $file . '" target="_blank">' . $file . '</a></li>';
+                    echo '</ul>';
+                }
             }
         }
-        echo '</ul>'
+    }
+    echo '</ul>'
     ?>
 </body>
+
 </html>
