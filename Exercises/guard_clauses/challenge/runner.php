504d83b5cca610eade5d9a4372d1d9d16edeee85
Johan Marx <johan.marx@stratusolve.com>
2022-09-23 10:16:18 +0200

    Added new exercises

--- Exercises/guard_clauses/challenge/runner.php
+++ Exercises/guard_clauses/challenge/runner.php
@@ -3,7 +3,7 @@ require_once('./function.php');
 
 $PostObj = new stdClass();
 $PostObj->Id = 234;
-$PostObj->Title = "My Inspiration!";
+// $PostObj->Title = "My Inspiration!";
 $PostObj->Content = "Everything can be taken from a man but one thing: the last of the human freedoms—to choose one’s attitude in any given set of circumstances, to choose one’s own way.";
 $PostObj->ImagePath = "image.jpg";
 $PostObj->Date = new DateTime('21 December 2020');
--- Exercises/guard_clauses/example/example.php
+++ Exercises/guard_clauses/example/example.php
@@ -1,21 +1,23 @@
 <?php
 
-function trySomething($a, $b, $c) {
-    if ($a) {
-        if ($b >= 3) {
-            if (!is_null($c)) {
-                doSomething();
-            } else {
-                die("C is null");
-            }
-        } else {
-            die("B is less than 3");
-        }
-    } else {
+function trySomething($a, $b, $c)
+{
+    if (!$a) {
         die("A is falsey");
     }
+
+    if ($b < 3) {
+        die("B is less than 3");
+    }
+
+    if (is_null($c)) {
+        die("C is null");
+    }
+
+    doSomething();
 }
 
-function doSomething() {
+function doSomething()
+{
     die("I did it");
 }
