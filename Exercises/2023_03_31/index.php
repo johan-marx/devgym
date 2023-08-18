5ad9759cdeef9954c81974ef021b20895ddc0aa1
Johan Marx <johan.marx@stratusolve.com>
2023-08-18 10:58:38 +0200

    Added some exercises

---
+++ Exercises/2023_03_31/index.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "sql";
+$username = "dxuser";
+$password = "secret";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
